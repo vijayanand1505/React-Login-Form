028fc04856433a0ee12587cd2290a2f0f69fff55
Vijay Anand <139949005+vijayanand1505@users.noreply.github.com>
2024-10-22 14:40:35 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "react-toastify/dist/ReactToastify.css"; 
+import "react-toastify/dist/ReactToastify.css";
 
 const Dashboard = () => {
   return (
